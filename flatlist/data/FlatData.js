4225ebc6c64255f03a23c426cd3210fe5a52f169
ProgrammerJayT <theanthem8@gmail.com>
2022-01-18 13:34:33 +0200

    Created FlatData

---
+++ flatlist/data/FlatData.js
@@ -0,0 +1,13 @@
+export default [
+    {
+        id: '0',
+        title: 'No 1',
+        image: require('_path'),
+    },
+
+    {
+        id: '1',
+        title: 'No 2',
+        image: require('_path'),
+    },
+]
