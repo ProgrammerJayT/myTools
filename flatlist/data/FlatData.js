f93745da8d2485685d6158aac4ef62372790facd
ProgrammerJayT <theanthem8@gmail.com>
2022-01-18 13:38:01 +0200

    modified image path

--- flatlist/data/FlatData.js
+++ flatlist/data/FlatData.js
@@ -2,12 +2,12 @@ export default [
     {
         id: '0',
         title: 'No 1',
-        image: require('_path'),
+        //image: require('_path'),
     },
 
     {
         id: '1',
         title: 'No 2',
-        image: require('_path'),
+        //image: require('_path'),
     },
 ]
