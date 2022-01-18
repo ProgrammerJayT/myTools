6103a9b10fb1a7403419c7e53ec1f17db99f1622
ProgrammerJayT <theanthem8@gmail.com>
2022-01-18 13:35:37 +0200

    Created FlattenerItem

---
+++ flatlist/components/FlatItem.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
+
+const FlatItem = () => {
+    return (
+        <SafeAreaView>
+            
+        </SafeAreaView>
+    )
+}
+
+export default FlatItem;
+
+const styles = StyleSheet.create({
+    body: {
+        flex: 1,
+        backgroundColor: '#fff',
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
+});
